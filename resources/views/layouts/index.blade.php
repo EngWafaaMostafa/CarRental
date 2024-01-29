c5849172b3dca7faa53845768ad5ee9893917c4f
wafaa mostafa <wafaa.mostafa.std@gmail.com>
2024-01-29 21:19:56 +0200

    first commit

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+
+@include('includes.head')
+
+<body>
+    @include('includes.header')
+    @include('includes.container')
+    @include('includes.site-section')
+    @include('includes.footer')
+    @include('includes.footerjs')
+
+</body>
+
+</html>
